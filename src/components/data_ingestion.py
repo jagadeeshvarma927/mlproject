4066baa5ccd58a79e1f24556f2063c853bcdc111
jagadeeshvarma927 <jagadeeshvarma927@gmail.com>
2026-02-12 11:45:59 +0530

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,7 +9,8 @@ from dataclasses import dataclass
 
 
 from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig   
+from src.components.data_transformation import DataTransformationConfig 
+from src.components.model_trainer import ModelTrainer  
 
 @dataclass
 class DataIngestionConfig:  
@@ -55,9 +56,12 @@ class DataIngestion:
             logging.info("Error occurred in data ingestion component")
             raise CustomException(e, sys)
     
-# if __name__ == "__main__":
-#     obj = DataIngestion()
-#     train_data, test_data = obj.initiate_data_ingestion()
+if __name__ == "__main__":
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
 
-#     data_transformation = DataTransformation()
-#     data_transformation.initiate_data_transformation(train_data, test_data)
+    data_transformation = DataTransformation()
+    train_arr, test_arr ,_  = data_transformation.initiate_data_transformation(train_data, test_data)
+    model_trainer = ModelTrainer()
+    
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
